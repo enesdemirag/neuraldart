d26c8dcf6fa25a92abeda1de3d355e517e8bfc8f
Enes Demirag <ensdmrg@gmail.com>
2020-04-03 13:46:57 +0300

    test actions

---
+++ test/network_test.dart
@@ -0,0 +1,12 @@
+import '../lib/src/neuralnetwork.dart';
+import '../lib/src/dataset.dart';
+
+void main() {
+  NeuralNetwork nn = new NeuralNetwork(5);
+  nn.layers[1] = Layer.namedConst(6,2);
+  nn.layers[2] = Layer.namedConst(12,6); 
+  nn.layers[3] = Layer.namedConst(6,12); 
+  nn.layers[4] = Layer.namedConst(1,6); 
+  
+  Dataset dataset = new Dataset();
+}
